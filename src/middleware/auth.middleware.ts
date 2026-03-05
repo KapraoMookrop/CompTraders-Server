033c5ff370bf60f40b7a0615de1b1a8d846c9c80
pitakarn99 <pitakarn_s@fissionsoft.co.th>
2026-03-05 15:45:32 +0700

    remove user

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -4,7 +4,7 @@ import { ENV } from "../config/env.js";
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader?.split(" ")[1];
 
   if (!token) {
     res.status(401).json({ message: "Access Token Required" });
